fbe230ff61ead6ab43deb65a3f56f3bdd5a16cee
OneAlon <>
2017-12-04 12:02:39 +0800

    添加UIView分类

---
+++ YABasicUIKit/Classes/UIView/UIView+Frame.h
@@ -0,0 +1,12 @@
+//
+//  UIView+Frame.h
+//  Pods-YABasicUIKit_Example
+//
+//  Created by OneAlon on 2017/12/4.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (Frame)
+
+@end
---
+++ YABasicUIKit/Classes/UIView/UIView+Frame.m
@@ -0,0 +1,12 @@
+//
+//  UIView+Frame.m
+//  Pods-YABasicUIKit_Example
+//
+//  Created by OneAlon on 2017/12/4.
+//
+
+#import "UIView+Frame.h"
+
+@implementation UIView (Frame)
+
+@end
